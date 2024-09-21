9e7bff1f4aaeadf680e88306d5c891f7d4c67d30
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-09-21 14:56:40 +0200

    [Feat (hasura)] init postgresql

--- README.md
+++ README.md
@@ -5,8 +5,22 @@ A quick playground displaying a graphql endpoint to handle stripe payment, with 
 Once docker compose is run, go to
 http://localhost:8080/console
 
+## Tracking migrations
+We need to use the console, which does the work of binding change made through the UI to local code file
+https://hasura.io/docs/2.0/hasura-cli/quickstart/ 
+
 # Express
 Pour build uniquement le service express
 ```bash
 docker compose build express-server
 ```
+
+## Stripe TODO
+
+Entrée de l'api : un événement ID (c'est lui qui détient les infos pour la répartition et les comptes connect)
+Il doit renvoyer une session de payment (checkout)
+
+multiple destination for a payment : https://chatgpt.com/share/66ed92b0-15dc-8002-9a5d-9d08108b96d8
+un seul payment vers l'entreprise (BeeWize), puis des transactions qui se déclacnhe par webhook lors d'un événement payment succeed
+
+ * gérér les échec de paiements
---
+++ migrations/default/1726923293280_run_sql_migration/up.sql
@@ -0,0 +1,27 @@
+create schema if not exists extensions;
+
+create extension if not exists "uuid-ossp" schema extensions;
+
+alter database "postgres"
+set
+    search_path to extensions,
+    public;
+
+
+create table if not exists
+    public.beewyse_ong (slug text primary key, stripe_customer_id text null, stripe_account_id text null, constraint slug check (slug ~ '^[a-zA-Z0-9_-]+$'));
+
+create table if not exists
+    public.beewyse_band (slug text primary key, constraint slug check (slug ~ '^[a-zA-Z0-9_-]+$'));
+
+create table if not exists
+    public.beewyse_artist (_id uuid not null default uuid_generate_v4 (), stripe_customer_id text null, stripe_account_id text null, constraint pk_artist primary key (_id));
+
+create table if not exists
+    public.beewyse_artist_band (
+        artist_id uuid not null,
+        band_id text not null,
+        primary key (artist_id, band_id),
+        constraint fk_artist foreign key (artist_id) references public.beewyse_artist (_id) on delete cascade,
+        constraint fk_band foreign key (band_id) references public.beewyse_band (slug) on delete cascade
+    );
